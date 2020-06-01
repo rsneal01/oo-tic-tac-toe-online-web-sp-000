e1eb51d5c33ba986a3466b9390d8697d638a4579
Bobby Neal <rsneal01@gmail.com>
2020-06-01 16:55:38 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -138,6 +138,14 @@ class TicTacToe
     end
   end
 
-
-  
-end
+  def play(board)
+    until over?(board)
+      turn(board)
+    end
+    if won?(board)
+      puts "Congratulations #{winner(board)}!"
+    elsif draw?(board)
+      puts "Cat's Game!"
+    end
+  end
+end  
